7bfa44b50ee106bfe355b4a2a10cf50660ecb90f
Scyllo <moondark.scyllo@gmail.com>
2025-08-26 23:07:54 +0200

    feat: setup pwa + docker watch

---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+useHead({
+  title: 'Application Formation Acadia',
+  meta: [
+    { name: 'description', content: 'Application pour aider à la formation de l\'association Acadia' }
+  ]
+})
+</script>
+
+<template>
+  <div>
+    BIENVENUE
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,33 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@vite-pwa/nuxt'
+  ],
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Application Formation Acadia',
+      short_name: 'FormationAcadia',
+      description: "Application pour aider à la formation de l'association Acadia",
+      theme_color: '#E9DAD8',
+      icons: [
+        {
+          src: 'ic_launcher_icon_192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'ic_launcher_icon_512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+      ]
+    },
+    devOptions: {
+      enabled: true
+    }
+  }
 })
